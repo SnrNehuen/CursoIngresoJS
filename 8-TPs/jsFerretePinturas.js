a7c259ea713a01c183e5e8364e3a5c58c049b67e
SnrNehuen <seniornehuen@hotmail.com>
2016-08-31 23:29:49 -0300

    Finalizado punto 3 TP (Ferreteria Pinturas)
    Con Angular.

--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,22 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
-{
-	
-}
 
-function CentigradosFahrenheit () 
+var logica=angular.module("FerretePinturas",[]);
+
+logica.controller("controlador", function($scope)
 {
-	
-}
+	$scope.form={};
+	$scope.resultado=0;
+
+	$scope.FahrenheitCentigrados=function()
+	{
+		$scope.resultado=$scope.form.temperatura+" Grados Fahrenheit son "+(((Number($scope.form.temperatura))-32)/1.8).toString()+" Grados Celsius.";
+	}
+
+	$scope.CentigradosFahrenheit=function()
+	{
+		$scope.resultado=$scope.form.temperatura+" Grados Celsius son "+((Number($scope.form.temperatura))*1.8+32).toString()+" Grados Fahrenheit.";
+	}
+
+});
