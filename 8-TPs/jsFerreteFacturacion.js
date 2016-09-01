12620fea5da4a5b90d6d78067e4c78c625b085f7
SnrNehuen <seniornehuen@hotmail.com>
2016-08-31 22:08:38 -0300

    Finalizado punto 1 TP (Ferreteria Facturación)
    Con Angular.

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,27 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
-function Sumar () 
+
+var logica=angular.module("FerreteFacturacion",[]);
+
+logica.controller("controlador", function($scope)
 {
-	
-}
-function Promedio () 
-{
-	
-}
-function PrecioFinal () 
-{
-	
-}
+	$scope.form={};
+	$scope.resultado=0;
+
+	$scope.Sumar=function()
+	{
+		$scope.resultado=Number($scope.form.numeroUno)+Number($scope.form.numeroDos)+Number($scope.form.numeroTres);
+	}
+
+	$scope.Promedio=function()
+	{
+		$scope.resultado=(Number($scope.form.numeroUno)+Number($scope.form.numeroDos)+Number($scope.form.numeroTres))/3;
+	}
+
+	$scope.PrecioFinal=function()
+	{
+		$scope.resultado=(Number($scope.form.numeroUno)+Number($scope.form.numeroDos)+Number($scope.form.numeroTres))*1.21;
+	}
+
+});
